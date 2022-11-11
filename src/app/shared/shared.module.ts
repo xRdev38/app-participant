8531d7458d2c036c0e7f17d18183e50b2da008c0
xRdev38 <developer.rudy@gmail.com>
2022-11-11 21:08:36 +0100

    chore: improve architecture

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseComponent } from './components';
+
+@NgModule({
+  declarations: [BaseComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
